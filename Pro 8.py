cc751d04ac9fe92c555d81a0a048cf053cf6de37
Harsha S <157780689+harshasnayaka@users.noreply.github.com>
2024-01-30 21:39:02 +0530

    Add files via upload

---
+++ Pro 8.py
@@ -0,0 +1,23 @@
+print("A small code about OTP : ")
+name = input("Enter your Name : ")
+if name == "Harsha":
+    print("You are Welcome !")
+else :
+    print("Hi , Stranger!")
+contact = int(input("Enter your Contact Number : "))
+print("OTP send Succesfully")
+print(" Duration : 5 min ")
+otp = int(input("Enter OTP : "))
+if otp == 5757:
+    print("Welcome!")
+else:
+    print("Invalid OTP!")
+print("Signing to your Account")
+print("Assume you are Booking Ticket Online ")
+website = input("Which website do you want to open ? ")
+print("Your Website is being Opened ")
+upi = int(input("Enter your UPI Pin "))
+if upi != 5757:
+    print("Sorry Invalid Transaction ")
+else :
+    print("Your Transaction is Being Processed ! ")
